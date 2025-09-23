06f9d98878acd1f9e1901d5d9439bee637e50ac8
Kanumarlapudi Satya Mounika <148631877+KanumarlapudiMounika@users.noreply.github.com>
2025-09-23 22:42:06 +0530

    Update auth.service.ts

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {jwtDecode} from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
+
 
 
 interface JwtPayload {
