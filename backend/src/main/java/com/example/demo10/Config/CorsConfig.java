6ad26be4a9186223d86329ce75035f68567d0394
Kanumarlapudi Satya Mounika <148631877+KanumarlapudiMounika@users.noreply.github.com>
2025-09-24 10:49:31 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/example/demo10/Config/CorsConfig.java
+++ backend/src/main/java/com/example/demo10/Config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 configuration.setAllowedOrigins(List.of("http://localhost:4200"
 		,
-		"https://think-frontend.azurewebsites.net")); // Allow Angular frontend
+		"https://gentle-rock-0cd317e00.2.azurestaticapps.net")); // Allow Angular frontend
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
